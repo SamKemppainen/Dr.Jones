c6fdcd552c43052733b8fd52c8592ac048a8b6da
SamKemppainen <isake006@edu.xamk.fi>
2026-01-29 15:19:58 +0200

    PlasmaShield for the other game

---
+++ Assets/GenMaterials/Import/Forcefield1.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forcefield2 : MonoBehaviour
+{
+    public GameObject ShieldEffect;
+    public float tilingSpeed = 1f; // Speed of tiling animation
+    private Material shieldMaterial;
+    private float tilingOffset = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShieldEffect.SetActive(false);
+        
+        // Get the material from the ShieldEffect's renderer
+        Renderer renderer = ShieldEffect.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            shieldMaterial = renderer.material;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ShieldEffect.SetActive(!ShieldEffect.activeSelf);
+        }
+        
+        // Continuously increase X and Y tiling when shield is active
+        if (ShieldEffect.activeSelf && shieldMaterial != null)
+        {
+            tilingOffset += tilingSpeed * Time.deltaTime;
+            shieldMaterial.mainTextureOffset = new Vector2(tilingOffset, tilingOffset);
+        }
+    }
+}
